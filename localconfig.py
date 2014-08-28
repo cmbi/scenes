57e6bf94bc4068b3e3da660372d6a589ac4186df
touwwouter <woutertouw@hotmail.com>
2014-08-28 11:33:14 +0200

    Add directory structure. Create whynot.check for now

--- localconfig.py
+++ localconfig.py
@@ -3,3 +3,11 @@
 
 # YASARA
 YASARA_DIR = '/home/wtouw/bin/yasara'
+
+# Root of scenes directory structure
+SCENES_ROOT = '/data0/scenes'
+
+# Scene dir names and file names
+# will be key/pdbid/pdbid_value.sce
+# keys should not be changed
+SCENES_NAME = {'ss2': 'sym-contacts'}
--- yas_scenes/application.py
+++ yas_scenes/application.py
@@ -3,11 +3,12 @@ _log = logging.getLogger(__name__)
 
 import argparse
 import errno
+import pyconfig
 import re
 import os
 
 from yas_scenes.parser import parse_sym_contacts
-from yas_scenes.tasks import ion_sites, symmetry_contacts
+from yas_scenes.tasks import symmetry_contacts
 
 
 PDB_ID_PAT = re.compile(r"^[0-9a-zA-Z]{4}$")
@@ -64,17 +65,29 @@ def ion(args):
     if created:
         _log.info('Scene created: {}'.format(scene))
     """
-    # TODO implement
+    _log.warn('Not yet implemented')
 
 
 def set_debug_loggers():
     # root logger
     logging.getLogger().setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s | %(levelname)-7s | %(message)s "
-                                  "[in %(pathname)s:%(lineno)d]")
+    formatter = logging.Formatter("%(asctime)s | %(levelname)-7s | %(message)s"
+                                  " [in %(pathname)s:%(lineno)d]")
     for handler in logging.getLogger().handlers:
         handler.setFormatter(formatter)
     _log.debug('Set verbose logging')
+
+
+def ensure_dir_existence(scene_dir):
+    """Create scene_dir if it does not exists.
+
+    Raise an OSError if the dir could not be created or is not writable, etc.
+    """
+    try:
+        os.makedirs(scene_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
 
 def ss2(args):
@@ -96,23 +109,30 @@ def ss2(args):
     log = 'logs/1crn_sym_contacts'
     symmetry_contacts(pdb, scene, sym_contacts, 123, log)
     """
-    # TODO modify as soon as we know desired directory structure
-    scene = 'sce/{}_sym_contacts.sce'.format(args.pdb_id)
+    scene_dir = os.path.join(pyconfig.get('SCENES_ROOT'), 'ss2', args.pdb_id)
+    ensure_dir_existence(scene_dir)
+    scene_nam = pyconfig.get('SCENES_NAME')
+    scene_nam = scene_nam['ss2']
+    scene = '{}_{}.sce'.format(args.pdb_id, scene_nam)
+    scene_path = os.path.join(scene_dir, scene)
+
     sym_contacts = parse_sym_contacts(ss2=args.ss2)
-    # TODO modify as soon as we know desired directory structure
-    log = 'logs/{}_sym_contacts'.format(args.pdb_id)
-    _log.info('Will try to create symmetry contacts YASARA scene {} from {} and'
-              ' {} for PDB ID {}'.format(scene, args.pdb_file_path, args.ss2,
-                                         args.pdb_id))
-    success, msg = symmetry_contacts(args.pdb_file_path, scene, sym_contacts,
-                                     args.ypid, log)
+
+    yas_log = '{}_{}'.format(args.pdb_id, scene_nam)
+    yas_log_path = os.path.join(scene_dir, yas_log)
+
+    _log.info('Will try to create symmetry contacts YASARA scene {} from {} '
+              'and {} for PDB ID {}'.format(scene_path, args.pdb_file_path,
+                                            args.ss2, args.pdb_id))
+    success, msg = symmetry_contacts(args.pdb_file_path, scene_path,
+                                     sym_contacts, args.ypid, yas_log_path)
     if not success:
         _log.error('{}: {}'.format(args.pdb_id, msg))
         # If the scene file is still present, delete it
         delete_scene(scene)
         # Create a WHY NOT entry
-        # TODO modify as soon as we know the DB name
-        file_path = 'sce/{}_sym_contacts.whynot'.format(args.pdb_id)
+        file_path = os.path.join(scene_dir, '{}_{}.whynot.check'.format(
+            args.pdb_id, scene_nam))
         write_whynot(args.pdb_id, msg, 'SCENE_SYMM_CON', file_path)
     else:
         _log.info('{}: {}'.format(args.pdb_id, msg))
@@ -141,12 +161,12 @@ def main():
     """Create YASARA scenes."""
 
     parser = argparse.ArgumentParser(
-        description="Create a YASARA scene. TO BE ADDED.")
+        description="Create a YASARA scene.")
     parser.add_argument("-v", "--verbose", help="show verbose output",
                         action="store_true")
     parser.add_argument("ypid", help="YASARA process id. Warning: specify a "
                         "different pid if multiple YASARA instances run on the"
-                        "same machine", type=int)
+                        " same machine", type=int)
     parser.add_argument("pdb_file_path", help="PDB file location.",
                         type=lambda x: is_valid_file(parser, x))
     parser.add_argument("pdb_id", help="PDB accession code.",
@@ -155,13 +175,13 @@ def main():
                                        description="YASARA scene type",
                                        help="ion for ion sites, symm for "
                                             "symmetry contacts")
-    p_ion = subparsers.add_parser("ion", description="Create a YASARA scene of "
-                                  "ion sites")
+    p_ion = subparsers.add_parser("ion", description="Create a YASARA scene of"
+                                  " ion sites")
     p_ion.add_argument("ion", help="WHAT IF list ion file (bzip2ed)",
                        type=lambda x: is_valid_file(parser, x))
     p_ion.set_defaults(func=ion)
-    p_ss2 = subparsers.add_parser("symm", description="Create a YASARA scene "
-                                  "with colored symmetry contacts")
+    p_ss2 = subparsers.add_parser("symm", description="Create a YASARA scene"
+                                  " with colored symmetry contacts")
     p_ss2.add_argument("ss2", help="WHAT IF list symmetry contacts file "
                                    "(bzip2ed), e.g. 1crn.ss2.bz2",
                        type=lambda x: is_valid_file(parser, x))
